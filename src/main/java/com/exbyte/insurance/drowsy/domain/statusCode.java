fb2cbe61fe614c669981b4d880b753002d9e4136
RAYOR <zihoyuno1@naver.com>
2020-03-12 23:29:31 +0900

    StatusCode 구현

---
+++ src/main/java/com/exbyte/insurance/drowsy/domain/statusCode.java
@@ -0,0 +1,20 @@
+package com.exbyte.insurance.drowsy.domain;
+
+public enum statusCode {
+
+	CODE_100(100, "눈깜빡임"),
+	CODE_101(101, "눈 감음"),
+	CODE_200(200, "하품"),
+	CODE_300(300, "운전자 이탈"),
+	CODE_301(301, "운전자 인식 실패"),
+	CODE_400(400, "정상");
+	
+	
+	private final int code;
+	private final String status;
+	
+	statusCode(int code, String status){
+		this.code = code;
+		this.status = status;
+	}
+}
